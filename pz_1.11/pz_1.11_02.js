55452d73c59f36e3987269602136281ae6169656
VESELIY1CHEL <voiteshenko2002@gmail.com>
2022-02-11 17:28:21 +0200

    Update 2 task

--- pz_1.11/pz_1.11_02.js
+++ pz_1.11/pz_1.11_02.js
@@ -1,10 +1,6 @@
 function task2(arr){
-    if(!Array.isArray(arr)){
-        console.log("isn`t a array!")  
-    }
-    else{
-        return arr.filter(value => {if(arr.indexOf(value)==arr.lastIndexOf(value)) return value;})[0];
-    }
+    if(!Array.isArray(arr))return "isn`t a array!";
+    return arr.filter(value => {if(arr.indexOf(value)==arr.lastIndexOf(value)) return value;})[0];
 }
 
 
